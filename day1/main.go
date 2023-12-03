8fea8b0788e7da6773c2a1be3162243ba915efbd
Okan Akyol <ok4nakyol@gmail.com>
2023-12-03 17:30:53 +0000

    feat: day 1 part 2

--- day1/main.go
+++ day1/main.go
@@ -6,9 +6,22 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 const FILE_PATH = "input.txt"
+
+var DIGITMAP = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
 
 func ReadFile() ([]string, error) {
 	file, err := os.Open(FILE_PATH)
@@ -30,17 +43,35 @@ func ReadFile() ([]string, error) {
 	return lines, nil
 }
 
-func firstDigit(num int) int {
+func FirstDigit(num int) int {
 	numStr := strconv.Itoa(num)
 	firstDigit := numStr[0] - '0'
-	// lastDigit := numStr[len(numStr)-1] - '0'
 	return int(firstDigit)
 }
 
-func lastDigit(num int) int {
+func LastDigit(num int) int {
 	numStr := strconv.Itoa(num)
 	lastDigit := numStr[len(numStr)-1] - '0'
 	return int(lastDigit)
+}
+
+func ReplaceSpelledDigits(line string) string {
+	re := regexp.MustCompile(`[a-zA-Z0-9]+`)
+	return re.ReplaceAllStringFunc(line, func(match string) string {
+		for i := 0; i < len(match); i++ {
+			for spelledDigit, digit := range DIGITMAP {
+				if strings.HasPrefix(match[i:], spelledDigit) {
+					before := match[:i]
+					after := match[i+len(spelledDigit):]
+					match = before + strconv.Itoa(digit) + after
+					// Update the index to skip past the newly replaced number
+					i += len(strconv.Itoa(digit)) - 1
+					break
+				}
+			}
+		}
+		return match
+	})
 }
 
 func main() {
@@ -52,15 +83,17 @@ func main() {
 	var numbers []int
 	for _, line := range lines {
 		regex := regexp.MustCompile(`\d+`)
-		matches := regex.FindAllString(line, -1)
+		fmt.Println("This is the line:", line)
+		replacedLine := ReplaceSpelledDigits(line)
+		fmt.Println("This is the replaced line:", replacedLine)
+		matches := regex.FindAllString(replacedLine, -1)
 		if len(matches) >= 1 {
 			firstMatch, _ := strconv.Atoi(matches[0])
 			lastMatch, _ := strconv.Atoi(matches[len(matches)-1])
-			merged := strconv.Itoa(firstDigit(firstMatch)) + strconv.Itoa(lastDigit(lastMatch))
+			merged := strconv.Itoa(FirstDigit(firstMatch)) + strconv.Itoa(LastDigit(lastMatch))
 
 			mergedInt, err := strconv.Atoi(merged)
 			if err != nil {
-				fmt.Println("Error converting string to int:", err)
 				return
 			}
 
@@ -69,12 +102,10 @@ func main() {
 		}
 	}
 
-	// sum all numbers
 	var sum int
 	for _, num := range numbers {
 		sum += num
 	}
 
 	fmt.Println("This is the sum:", sum)
-
 }
